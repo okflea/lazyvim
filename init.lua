7d95750941eca19e2f096c66d458b29b475fe2ca
okflea <rohit.1.biswas.1@gmail.com>
2023-12-26 20:18:43 +0530

    keymaps , misc

--- init.lua
+++ init.lua
@@ -1,4 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.o.guifont= "CaskaydiaCove Nerd Font:h9"
+-- for neovide
 
+if vim.g.neovide then
+  vim.g.neovide_scale_factor = 0.9
+  vim.g.neovide_transparency = 0.6
+  vim.g.transparency = 0.6
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
+
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 10
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
+end
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
-vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
-vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+-- vim.keymap.set("n", "<M-1>", "<cmd>ToggleTerm<CR>", { desc = "Toggle bottom terminal" })
+-- vim.keymap.set("n", "<M-1>", "<cmd>ToggleTerm<CR>", { desc = "Toggle bottom terminal" })
+vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns blame_line<cr>", { desc = "Git blame line" })
